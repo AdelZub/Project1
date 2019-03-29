8ad84ed80c459fa80dee1b3e33fe7e562e291223
AdelZub <47501386+AdelZub@users.noreply.github.com>
2019-03-29 17:33:04 +0500

    Update Project.java

--- Nado/src/Project.java
+++ Nado/src/Project.java
@@ -1,10 +1,14 @@
 
 public class Project {
+	/* опознание класса
 
 	public static void main(String[] args) {
+	/* главная функция 
 		
 	System.out.println("Grigoreva, Zubairova");
+		/* вывод сообщения на экран
 
 	}
 
 }
+/* завершение выполнения функции
