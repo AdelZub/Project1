98002e57282467fda06e6c87490671e33a0f8dc9
AdelZub <47501386+AdelZub@users.noreply.github.com>
2019-03-29 17:04:03 +0500

    Update Project.java

--- Nado/src/Project.java
+++ Nado/src/Project.java
@@ -1,5 +1,8 @@
 
 public class Project {
+	/* Это коллективная java-программа.
+   В результате выполнения на экран будет выведено 'список группы №2'
+   */
 
 	public static void main(String[] args) {
 		
